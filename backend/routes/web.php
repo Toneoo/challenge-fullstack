d724143b2994ece166954a7ad5e227270a9365e0
Toneoo <lucasjordaobaltazar@gmail.com>
2024-12-02 16:39:22 -0300

    Alterações feitas por lucas

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('produtos', [ProductController::class, 'index']);
+Route::get('produtos', [ProductController::class, 'index']);
